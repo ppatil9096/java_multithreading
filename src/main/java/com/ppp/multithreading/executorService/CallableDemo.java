b44a04e75a7fa301979958a9683ea8d00372bb19
ppatil9096 <ppp@gmail.com>
2025-02-09 19:43:03 +0530

    ExecutorService -> Future class functions

--- src/main/java/com/ppp/multithreading/executorService/CallableDemo.java
+++ src/main/java/com/ppp/multithreading/executorService/CallableDemo.java
@@ -3,10 +3,15 @@ package com.ppp.multithreading.executorService;
 import java.util.concurrent.*;
 
 public class CallableDemo {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         try (ExecutorService service = Executors.newFixedThreadPool(4)) {
             Future<Boolean> value = service.submit(new TaskB());
             System.out.println(value.get());
+//            value.get(1000, TimeUnit.MILLISECONDS); //throws TimeoutException if we didn't get results within specified time
+//            value.cancel(true);
+//            value.isCancelled();
+//            value.isDone();
+
         }
     }
 }
